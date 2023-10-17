3e763e261bf08fb2d24cb4198abc7d706908d7c8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-17 12:07:05 +0300

    Got rid of all the elements in the style object

--- App.js
+++ App.js
@@ -5,11 +5,4 @@ export default function App() {
 	return <StartGameScreen />
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
-})
+const styles = StyleSheet.create({})
