9fe19adcedf0a3ecce5fc4f2ad4b12743dd1dd2d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-17 12:06:33 +0300

    Rendered the status bar in the StartGameScreen componewnr

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,9 +1,11 @@
+import { StatusBar } from 'react-native'
 import { TextInput } from 'react-native'
 import { View } from 'react-native'
 
 const StartGameScreen = () => {
 	return (
 		<View>
+			<StatusBar style='auto' />
 			<TextInput />
 			{/* TODO Add Buttons */}
 		</View>
