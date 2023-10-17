d7476e2058275c46f60d92fe9253fcf1402b5590
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-17 16:14:53 +0300

    Rendering the children of the component

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,7 +1,12 @@
+import { Text } from 'react-native'
 import { View } from 'react-native'
 
-const NumberContainer = () => {
-	return <View></View>
+const NumberContainer = ({ children }) => {
+	return (
+		<View>
+			<Text>{children}</Text>
+		</View>
+	)
 }
 
 export default NumberContainer
