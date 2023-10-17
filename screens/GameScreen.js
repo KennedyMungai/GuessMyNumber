8b0f4c2af0ebe610cf432c99b273697d12178a04
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-17 15:23:51 +0300

    Added some placeholder for the GameScreen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -3,7 +3,13 @@ import { Text, View } from 'react-native'
 const GameScreen = () => {
 	return (
 		<View>
-			<Text>Game Screen</Text>
+			<Text>Opponent's Guess</Text>
+			{/* TODO: Guess */}
+			<View>
+				<Text>Higher or Lower?</Text>
+				{/* TODO: Plus and Minus button to show if higher or lower */}
+			</View>
+			<View>{/* TODO: Logging the rounds of the game */}</View>
 		</View>
 	)
 }
