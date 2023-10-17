23d402e1e59fea88c12f3045efe2921329db0071
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-17 12:09:08 +0300

    Added the Reset and Confirm buttons to the StartGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,13 +1,15 @@
 import { StatusBar } from 'react-native'
 import { TextInput } from 'react-native'
 import { View } from 'react-native'
+import PrimaryButton from '../components/PrimaryButton'
 
 const StartGameScreen = () => {
 	return (
 		<View>
 			<StatusBar style='auto' />
 			<TextInput />
-			{/* TODO Add Buttons */}
+			<PrimaryButton>Reset</PrimaryButton>
+			<PrimaryButton>Confirm</PrimaryButton>
 		</View>
 	)
 }
