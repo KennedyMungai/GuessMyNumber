aed92cb73138bfb43b5099b95dbc61f7d6b6ed1b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-17 12:14:54 +0300

    Aligned the items in the startGameScreen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,11 @@
 import { TextInput } from 'react-native'
 import { View } from 'react-native'
 import PrimaryButton from '../components/PrimaryButton'
+import { StyleSheet } from 'react-native'
 
 const StartGameScreen = () => {
 	return (
-		<View>
+		<View style={styles.startGameView}>
 			<TextInput />
 			<PrimaryButton>Reset</PrimaryButton>
 			<PrimaryButton>Confirm</PrimaryButton>
@@ -13,3 +14,13 @@ const StartGameScreen = () => {
 }
 
 export default StartGameScreen
+
+const styles = StyleSheet.create({
+	startGameView: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		alignItems: 'center',
+		gap: 10
+	}
+})
