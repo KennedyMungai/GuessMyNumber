e005d66ab2afc03bba7d42aca5f18258323d4133
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-18 09:10:32 +0300

    Imported the useEffect hook

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Alert, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import NumberContainer from '../components/game/NumberContainer'
 import PrimaryButton from '../components/ui/PrimaryButton'
